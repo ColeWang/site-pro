b2cd0cef8dbed29cebd9318a4aa71e2bfa3cbeeb
cole <cole@google.com>
2024-08-27 01:43:34 +0800

    wip: components

--- packages/components/examples/views/Home.vue
+++ packages/components/examples/views/Home.vue
@@ -8,7 +8,7 @@
 
 <script setup lang="ts">
   import { BaseForm as ProBaseForm, LocaleProvider, Text as ProText } from '@site-pro/components'
-  import { enUS } from '@site-pro/locale'
+  import enUS from '@site-pro/locale/en-US'
   
   function onFinish (value: string) {
     return value
