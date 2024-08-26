cdcc888573a4e2a99083668ecd49bfeb474310f7
cole <cole@google.com>
2024-08-27 01:10:25 +0800

    wip: components

--- packages/locale/vite.config.ts
+++ packages/locale/vite.config.ts
@@ -5,7 +5,7 @@ import { dirname, resolve } from 'path'
 import { defineConfig, loadEnv } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueJsx from '@vitejs/plugin-vue-jsx'
-import typescript from '@rollup/plugin-typescript'
+import dts from 'vite-plugin-dts'
 
 const __filename: string = fileURLToPath(import.meta.url)
 const __dirname: string = dirname(__filename)
@@ -34,7 +34,11 @@ export default defineConfig((config) => {
     return {
         plugins: [
             vue(),
-            vueJsx()
+            vueJsx(),
+            dts({
+                tsconfigPath: './tsconfig.dts.json',
+                outDir: 'types'
+            })
         ],
         resolve: {
             alias: {
@@ -42,6 +46,7 @@ export default defineConfig((config) => {
             }
         },
         build: {
+            target: 'modules',
             minify: false,
             lib: {
                 entry: resolve(__dirname, 'src')
@@ -58,25 +63,22 @@ export default defineConfig((config) => {
                 ],
                 output: [
                     {
-                        entryFileNames: (chunkInfo) => {
-                            return chunkInfo.name + '.esm.js'
-                        },
-                        format: 'es'
+                        format: 'es',
+                        dir: 'es',
+                        exports: 'named',
+                        entryFileNames: '[name].js',
+                        preserveModules: true,
+                        preserveModulesRoot: 'src'
                     },
                     {
-                        entryFileNames: (chunkInfo) => {
-                            return chunkInfo.name + '.cjs.js'
-                        },
                         format: 'cjs',
+                        dir: 'lib',
+                        exports: 'named',
+                        entryFileNames: '[name].js',
+                        preserveModules: true,
+                        preserveModulesRoot: 'src',
                         paths: replacePaths
                     }
-                ],
-                plugins: [
-                    typescript({
-                        tsconfig: './tsconfig.esm.json',
-                        target: 'es2020',
-                        emitDeclarationOnly: true,
-                    })
                 ]
             }
         },
