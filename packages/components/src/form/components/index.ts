57be1b0eeaa5aba7a7875525447bdd8f054ea4e9
cole <cole@google.com>
2024-08-27 15:25:31 +0800

    wip: components

--- packages/components/src/form/components/index.ts
+++ packages/components/src/form/components/index.ts
@@ -1,9 +1,78 @@
-import Field, { fieldProps } from './Field'
-
-export { Field, fieldProps }
+export { default as Field, fieldProps } from './Field'
 export type { FieldSizeType, FieldProps, FieldInstance } from './Field'
+
+// -- --
+export { default as Date, dateProps } from './Date'
+export type { DateProps, DateInstance } from './Date'
+
+export { default as DateRange, dateRangeProps } from './DateRange'
+export type { DateRangeProps, DateRangeInstance } from './DateRange'
+
+export { default as DateWeek, dateWeekProps } from './DateWeek'
+export type { DateWeekProps, DateWeekInstance } from './DateWeek'
+
+export { default as DateWeekRange, dateWeekRangeProps } from './DateWeekRange'
+export type { DateWeekRangeProps, DateWeekRangeInstance } from './DateWeekRange'
+
+export { default as DateMonth, dateMonthProps } from './DateMonth'
+export type { DateMonthProps, DateMonthInstance } from './DateMonth'
+
+export { default as DateMonthRange, dateMonthRangeProps } from './DateMonthRange'
+export type { DateMonthRangeProps, DateMonthRangeInstance } from './DateMonthRange'
+
+export { default as DateQuarter, dateQuarterProps } from './DateQuarter'
+export type { DateQuarterProps, DateQuarterInstance } from './DateQuarter'
+
+export { default as DateQuarterRange, dateQuarterRangeProps } from './DateQuarterRange'
+export type { DateQuarterRangeProps, DateQuarterRangeInstance } from './DateQuarterRange'
+
+export { default as DateYear, dateYearProps } from './DateYear'
+export type { DateYearProps, DateYearInstance } from './DateYear'
+
+export { default as DateYearRange, dateYearRangeProps } from './DateYearRange'
+export type { DateYearRangeProps, DateYearRangeInstance } from './DateYearRange'
+
+export { default as DateTime, dateTimeProps } from './DateTime'
+export type { DateTimeProps, DateTimeInstance } from './DateTime'
+
+export { default as DateTimeRange, dateTimeRangeProps } from './DateTimeRange'
+export type { DateTimeRangeProps, DateTimeRangeInstance } from './DateTimeRange'
+
+export { default as Time, timeProps } from './Time'
+export type { TimeProps, TimeInstance } from './Time'
+
+export { default as TimeRange, timeRangeProps } from './TimeRange'
+export type { TimeRangeProps, TimeRangeInstance } from './TimeRange'
+
+export { default as Select, selectProps } from './Select'
+export type { SelectProps, SelectInstance } from './Select'
+
+export { default as TreeSelect, treeSelectProps } from './TreeSelect'
+export type { TreeSelectProps, TreeSelectInstance } from './TreeSelect'
 
 export { default as Cascader, cascaderProps } from './Cascader'
 export type { CascaderProps, CascaderInstance } from './Cascader'
+
+export { default as Radio, radioProps } from './Radio'
+export type { RadioProps, RadioInstance } from './Radio'
+
+export { default as Checkbox, checkboxProps } from './Checkbox'
+export type { CheckboxProps, CheckboxInstance } from './Checkbox'
+
+export { default as Switch, switchProps } from './Switch'
+export type { SwitchProps, SwitchInstance } from './Switch'
+
+export { default as Slider, sliderProps } from './Slider'
+export type { SliderProps, SliderInstance } from './Slider'
+
 export { default as Text, textProps } from './Text'
 export type { TextProps, TextInstance } from './Text'
+
+export { default as Number, numberProps } from './Number'
+export type { NumberProps, NumberInstance } from './Number'
+
+export { default as Textarea, textareaProps } from './Textarea'
+export type { TextareaProps, TextareaInstance } from './Textarea'
+
+export { default as Password, passwordProps } from './Password'
+export type { PasswordProps, PasswordInstance } from './Password'
