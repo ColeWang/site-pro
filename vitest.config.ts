d39afefe843cef67c4d3046246734e7e0c431f8c
cole <cole@google.com>
2024-12-23 01:48:12 +0800

    feat: init tests

---
+++ vitest.config.ts
@@ -0,0 +1,22 @@
+import { defineConfig, mergeConfig } from 'vitest/config'
+
+export default defineConfig((configEnv) => {
+    return mergeConfig(viteConfig(configEnv), defineConfig({
+        test: {
+            environment: 'jsdom',
+            setupFiles: ['./packages/tests/setup.js'],
+            include: ['**/*.{test,spec,type-test}.{js,mjs,cjs,ts,tsx,jsx}'],
+            coverage: {
+                exclude: [
+                    'packages/tests/**',
+                    'packages/index.js',
+                    '.eslintrc.cjs',
+                    'entry.packages.js',
+                    'vite.config.js',
+                    'vite.packages.config.js',
+                    'src/**',
+                ]
+            }
+        }
+    }))
+})
