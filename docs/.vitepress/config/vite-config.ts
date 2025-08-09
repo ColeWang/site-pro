cdcfff0ff8d1b945b865edc81664b0adc77ef653
cole <cole@google.com>
2025-08-09 18:25:18 +0800

    docs: config

--- docs/.vitepress/config/vite-config.ts
+++ docs/.vitepress/config/vite-config.ts
@@ -1,16 +1,14 @@
 import { defineConfig } from 'vite'
 import vueJsx from '@vitejs/plugin-vue-jsx'
 
-export default defineConfig(() => {
-    return {
-        plugins: [
-            vueJsx()
-        ],
-        ssr: {
-            noExternal: [
-                'ant-design-vue',
-                '@ant-design/icons-vue'
-            ]
-        }
+export default defineConfig({
+    plugins: [
+        vueJsx()
+    ],
+    ssr: {
+        noExternal: [
+            'ant-design-vue',
+            '@ant-design/icons-vue'
+        ]
     }
 })
