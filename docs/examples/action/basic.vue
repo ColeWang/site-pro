4b25a57a7649467eade4d3ed70a67ebd2263f2ce
cole <cole@google.com>
2025-08-12 13:06:45 +0800

    test: version 1.0.0-alpha.2

--- docs/examples/action/basic.vue
+++ docs/examples/action/basic.vue
@@ -3,5 +3,6 @@
         <pro-action>选项一</pro-action>
         <pro-action>选项二</pro-action>
         <pro-action>选项三</pro-action>
+        <pro-checkbox></pro-checkbox>
     </pro-action-group>
 </template>
